70c5e121289c65c03bc6f455cc262c78802d1961
Abhinav Dadhich <abhinavvicku@gmail.com>
2014-10-08 15:17:09 +0900

    Create octomap_sub.h

---
+++ dynamic_object/src/octomap_sub.h
@@ -0,0 +1,38 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ */
+
+
+
+#include <iostream>
+#include "ros/ros.h"
+#include "sensor_msgs/PointCloud2.h"
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/conversions.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl/filters/filter.h>
+#include <octomap_msgs/conversions.h>
+#include <octomap_msgs/Octomap.h>
+#include <deque>
+#include <pcl/octree/octree.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl/filters/passthrough.h>
