f00701a44859509a341d991b3899cec66835a3a5
Abhinav Dadhich <abhinavvicku@gmail.com>
2014-10-08 15:16:36 +0900

    inserted GPL

--- dynamic_object/src/octomap_sub.cpp
+++ dynamic_object/src/octomap_sub.cpp
@@ -1,21 +1,25 @@
-#include <iostream>
-#include "ros/ros.h"
-#include "sensor_msgs/PointCloud2.h"
-#include <octomap/octomap.h>
-#include <octomap/OcTree.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <pcl/conversions.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/PCLPointCloud2.h>
-#include <pcl/filters/filter.h>
-#include <octomap_msgs/conversions.h>
-#include <octomap_msgs/Octomap.h>
-#include <deque>
-#include <pcl/octree/octree.h>
-#include <pcl/filters/statistical_outlier_removal.h>
-#include <pcl_ros/point_cloud.h>
-#include <pcl/filters/passthrough.h>
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ */
+
+
+
+#include "octomap_sub.h"
+
 std::deque< pcl::PointCloud<pcl::PointXYZ> > cloud_seq_loaded; 
 const float res = 0.1;
 pcl::PointCloud<pcl::PointXYZ> static_pc;
@@ -54,7 +58,7 @@ void processCloud(const sensor_msgs::PointCloud2 msg)
 	//*********** Process currently observerd and buffered data*********
 
 	if(cloud_seq_loaded.size()==2){
-		std::cout<< "Generating octomap"<<std::endl;
+		
 		
 		*prev_pc =static_pc; //cloud_seq_loaded.front(); ss
 		
