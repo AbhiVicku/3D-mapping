6f4c12eb5709c84f47fe97aa67f268973ae4f668
Abhinav Dadhich <abhinavvicku@gmail.com>
2014-11-14 21:30:35 +0900

    Create poseGraph.h

---
+++ Pose_Graph/poseGraph.h
@@ -0,0 +1,40 @@
+// header file for poseGraph.cpp
+
+#ifndef __POSEGRAPH_H_INCLUDED__  // if this header is not included yet
+#define __POSEGRAPH_H_INCLUDED__  // then include this header
+
+#include <iostream>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <Eigen/Dense>
+#include <boost/graph/graphviz.hpp>
+
+class PoseGraph{
+	private:
+		struct pose_{
+			int key;
+			std::vector<double>  data;
+		};
+		
+		// Custom edge properties as constraints
+		struct constraints_{
+			Eigen::Matrix4f transformation;
+		};
+		
+		//Graph type 
+		typedef boost::adjacency_list<boost::listS,boost::vecS, boost::undirectedS,pose_, constraints_> Graph_;
+		typedef boost::graph_traits<Graph_>::vertex_descriptor Vertex_;
+		typedef boost::graph_traits<Graph_>::edge_descriptor Edge_;
+		Graph_ gr_;
+		
+		public:
+		// Add vertex to the graph
+			void addVertex(int v);
+		// Add edge in betwwen the last 2 vertices of the graph
+			void addEdgeToPrev(Eigen::Matrix4f tr_msg);
+		// Print the graph
+			void display();
+};
+
+#endif
