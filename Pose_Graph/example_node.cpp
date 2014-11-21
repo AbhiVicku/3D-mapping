106290d605fe750ac0da02a6a4e1c7f50a483dcd
Abhinav Dadhich <abhinavvicku@gmail.com>
2014-11-21 10:48:13 +0900

    updated the pose graph and added initialization

--- Pose_Graph/example_node.cpp
+++ Pose_Graph/example_node.cpp
@@ -11,7 +11,8 @@
 int main(int argc, char **argv)
 {
 	/* code */
-	int count = 0;
+	int count ;
+	bool first;
 	Eigen::Matrix4f tr_mat;
 	ros::init(argc,argv,"example_node");
 	OdomProcess od_p;
@@ -20,22 +21,41 @@ int main(int argc, char **argv)
 	// Create initial subscriber 
 	od_p.subr();
 	pcl_p.subr();		
-
+	first = true;
+	count = 0;
+	
 	/* while the process is going on do the stuff */
 	while (ros::ok()){
 		std::vector<double> odom_data;
 		odom_data.push_back(od_p.pose_x);
 		odom_data.push_back(od_p.pose_y);
 		odom_data.push_back(od_p.yaw);		
-		pg.addVertex(count,odom_data);
-		if(pcl_p.cloud_seq_loaded.size()==2){
-			
-			pcl_p.calcICP();
-			//pg.addEdgeToPrev(tr_mat);
-			//std::cout << tr_mat << std::endl;
+		
+		if(pcl_p.cloud_seq_loaded.size()==1){
+			std::cout<<"Initalizing pose graph"<< std::endl;
+			std::vector<double> odom_init;
+			odom_init.push_back(od_p.pose_x);
+			odom_init.push_back(od_p.pose_y);
+			odom_init.push_back(od_p.yaw);	
+
+			pg.addVertex(count, odom_init);
 		}
-		//pg.display();
-		count++;
+
+		if(pcl_p.cloud_seq_loaded.size()==2){
+				std::cout<<"count is :"<< count<< std::endl;
+				
+				tr_mat = pcl_p.calcICP();
+				//std::cout << pcl_p.calcICP() <<std::endl;
+				count++;
+				pg.addVertex(count,odom_data);
+				pg.addEdgeToPrev(tr_mat);
+				//std::cout << tr_mat << std::endl;
+				
+		}
+		
+
+		pg.display();
+		
 		ros::spinOnce();
 	}
 	
