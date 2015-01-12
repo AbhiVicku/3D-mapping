87a6150552ea982691513b4b46fae6b0b831a996
Abhinav Dadhich <abhinavvicku@gmail.com>
2015-01-12 13:01:43 +0900

    updated comments

--- Pose_Graph/example_node.cpp
+++ Pose_Graph/example_node.cpp
@@ -8,68 +8,99 @@
 #include "poseGraph.h"
 #include <Eigen/Dense>
 #include <fstream>
+#include <cmath>
 
 
 int main(int argc, char **argv)
 {
-	std::cout << "----------------------------------------------- "<<std::endl;
-	std::cout << "-------------This is a pose graph slam--------- "<<std::endl;
-	std::cout << "----------------------------------------------- "<<std::endl;
-	
+	// Pose graph SLAM main Node. 
+
+	/**************** Defining variables ************/	
 	std::ofstream myfile;
-	/* code */
 	myfile.open ("example.txt");
 	int count ;
 	bool first;
+	float dist ;
+	float prev_odom[2];
 	Eigen::Matrix4f tr_mat;
-	ros::init(argc,argv,"example_node");
+
+
+	/****************Initializing Ros components*************/
+	ros::init(argc,argv,"pose_graph_node");
+
+	// Starting the odometry process to fetch the data.
 	OdomProcess od_p;
+	// Starting the pointcloud process, to get the cloud data. 
 	PointCloudProcessor pcl_p;
+	// Initializing the graph struture of the pose graph 
 	PoseGraph pg;
-	// Create initial subscriber 
+	// Starting the subcribers for incoming data.
 	od_p.subr();
 	pcl_p.subr();		
+
+	/****************** Initializing local variables ************/
 	first = true;
 	count = 0;
-	
+	dist = 0.0;
+
+
 	/* while the process is going on do the stuff */
 	while (ros::ok()){
 	
+		// Recieve the odometry data for current Iteration. what is the use ??.
 		std::vector<double> odom_data;
 		odom_data.push_back(od_p.pose_x);
 		odom_data.push_back(od_p.pose_y);
 		odom_data.push_back(od_p.yaw);		
 		
+		/* For the first cloud recieved initialize vertex ********/
 		if((pcl_p.cloud_seq_loaded.size()==1) && first){
 			std::cout<<"-->Initalizing pose graph"<< std::endl;
 			std::vector<double> odom_init;
 			odom_init.push_back(od_p.pose_x);
 			odom_init.push_back(od_p.pose_y);
 			odom_init.push_back(od_p.yaw);	
-
+			prev_odom[0] = od_p.pose_x;
+			prev_odom[1] = od_p.pose_y;
+			// add the odometry data to the current vertex
 			pg.addVertex(count, odom_init);
 			first = false;
 		}
 
+		/* On recieving data , compute */
 		if(pcl_p.cloud_seq_loaded.size()==2){
+			float curr_odom[2];
+			curr_odom[0] = od_p.pose_x;
+			curr_odom[1] = od_p.pose_y;
+			// calculate the distance
+			dist = pow((curr_odom[0] - prev_odom[0]),2) + pow((curr_odom[1]-prev_odom[1]),2);
+			dist = sqrt(dist);
+			// if robot traverses a distance greater than 0.5 m then add the next node.
+			if (dist>= 0.5){
+				prev_odom[0] = curr_odom[0];
+			 	prev_odom[1] = curr_odom[1];
 				std::cout<<"Count is :"<< count<< std::endl;
-				
-				tr_mat = pcl_p.calcICP();
+				// estimate the transition matrix for the current postion wrt to previous position saved. 
+				tr_mat = pcl_p.estTrans();
 				//std::cout << pcl_p.calcICP() <<std::endl;
 				count++;
+				// add the data as vertex to graph and update the edge weight with transformation matrix.
 				pg.addVertex(count,odom_data);
 				pg.addEdgeToPrev(tr_mat);
 				//std::cout << tr_mat << std::endl;
-				
+			}	
 		}
 		
-
-		pg.display();
+		// To display the graph struture created.
+		//pg.display();
 		
+		// While the ros is reciving data continiously subcribe to different topics
 		ros::spinOnce();
 	}
 	
+	// In order to generate file corresponding to the graph structure.
 	
+	/* First write vertex onto file */
 	boost::tie(pg.vertexIt_, pg.vertexEnd_) = boost::vertices(pg.gr_);
 	for(;pg.vertexIt_!=pg.vertexEnd_;++pg.vertexIt_)
 	{
@@ -78,13 +109,15 @@ int main(int argc, char **argv)
 		myfile<<" ";
 		for (int i =0;i< 3;i++){
 			myfile<<pg.gr_[*pg.vertexIt_].data[i];
-			std::cout<<pg.gr_[*pg.vertexIt_].data[i]<<std::endl;;
+			//std::cout<<pg.gr_[*pg.vertexIt_].data[i]<<std::endl;;
 			myfile<<" ";
 			}
 		myfile<<"\n";
 		
 		std::cout<<std::endl;
 		}
+
+	/* Write edges to same file */	
 	boost::tie(pg.edgeIt_,pg.edgeEnd_) = boost::edges(pg.gr_);
 	for(;pg.edgeIt_!=pg.edgeEnd_;++pg.edgeIt_)
 	{
@@ -113,6 +146,7 @@ int main(int argc, char **argv)
 		std::cout<<*neighbourIt<<" ";
 	*/
 	
+	// close file (Important)
 	myfile.close();
 	
 	return 0;
