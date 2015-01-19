9b04a31dc83fff2dd004f15d2b1e4de697b21078
Abhinav Dadhich <abhinavvicku@gmail.com>
2015-01-19 17:45:15 +0900

    Update node.cpp

--- Pose_Graph/node.cpp
+++ Pose_Graph/node.cpp
@@ -1,7 +1,7 @@
 /*
  * node.cpp
  * 
- * Copyright 2014 abhinav <abhinav@abhinav-VirtualBox>
+ * Copyright 2015 Shibata-Lab <shibata-lab@shibatalab-X500H>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,17 +18,13 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
  * 
- * 
+ * NOTE: This is written according to ROS c++ guidelines
  */
 
 
 #include <iostream>
 #include <ros/ros.h>
-#include <geometry_msgs/Pose.h>
 #include <nav_msgs/Odometry.h>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/graph_utility.hpp>
-#include <boost/property_map/property_map.hpp>
 #include <tf/transform_datatypes.h>
 #include "sensor_msgs/PointCloud2.h"
 #include <pcl/io/pcd_io.h>
@@ -37,212 +33,253 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl_ros/point_cloud.h>
-#include <vector>
-#include <deque>
 #include <pcl/filters/filter.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/random_sample.h>
+#include <pcl/registration/transforms.h> 
+#include <pcl/registration/transformation_estimation_2D.h>
 #include <pcl/registration/icp.h>
+#include <Eigen/Dense>
+#include <deque>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_utility.hpp>
 #include <boost/property_map/property_map.hpp>
-#include <Eigen/Dense>
 #include <boost/graph/graphviz.hpp>
 
+using namespace std;
 
-typedef std::deque< pcl::PointCloud<pcl::PointXYZ> > cloud_seq;
-typedef std::deque<std::list<double> > pose_seq;
-//create custom vertex property
-struct Pose{
-	int key;
-	std::list<double>  data;
+class Node{
+	public:
+		float threshold_distance;
+		ros::NodeHandle nh;
+		ros::Subscriber odom;
+		ros::Subscriber cloud;
+		
+		double pose_x,pose_y,roll,pitch,yaw;
+		pcl::PCLPointCloud2::Ptr cloud_ptr;
+		pcl::PCLPointCloud2 cloud_ ; 
+		pcl::PointCloud<pcl::PointXYZ> curr_pc;
+		Eigen::Matrix4f tr_mat;
+		Eigen::Matrix4f prev_tr_mat;
+		// properties for vertex of the graph 
+		struct pose_{
+			int key;
+			std::vector<double>  data;
+		};
+		
+		// Custom edge properties as constraints
+		struct constraints_{
+			int src;
+			int obs;
+			Eigen::Matrix4f transformation;
+		};
+		//Graph type 
+		typedef boost::adjacency_list<boost::listS,boost::vecS, boost::undirectedS,pose_, constraints_> Graph;
+		typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+		typedef boost::graph_traits<Graph>::edge_descriptor Edge;
+		Graph gr;
+		Graph::vertex_iterator vertex_It,vertex_End;
+		Graph::edge_iterator edge_It,edge_End;
+		
+		Node(); //constructor
+		/* Get Odometry data */
+		void odomSub();
+		
+		/* procees odometry data to get 2D pose */
+		void odomCallbk(nav_msgs::Odometry odom_data);
+		
+		/* Get Point Cloud data */
+		void cloudSub();
+		
+		/* process point cloud data */ 
+		void cloudCallbk(sensor_msgs::PointCloud2 msg);
+		
+		/* calculate Transform between two point clouds */ 
+		Eigen::Matrix4f estTrans(pcl::PointCloud<pcl::PointXYZ> first,pcl::PointCloud<pcl::PointXYZ> second);	
+		pcl::PointCloud<pcl::PointXYZ> randomSample(pcl::PointCloud<pcl::PointXYZ> in_cld);
+	
+		
+		/* calculate distance travelled */ 
+		float calculateDist(vector<double> curr_odom,vector<double> prev_odom);
+		
+		/* Add vertex to graph with given data */ 
+		void addVertex(int v,std::vector<double> odom);
+		
+		/* Add edge to the last added vertex */ 
+		void addEdge(Eigen::Matrix4f tr_msg);
+		
+		/* Initialize graph */ 
+		void initGraph(int v,std::vector<double> odom);
+		
+		/* main node process */ 
+		
+	
+	
 	};
-struct constraints{
-	Eigen::Matrix4f transformation;
+/* Callback function for odom subscriber */	
+void Node::odomCallbk(nav_msgs::Odometry odom_data){
+	/* Calculate quaternion to get roll, pitch , yaw */
+	tf::Quaternion q(odom_data.pose.pose.orientation.x, odom_data.pose.pose.orientation.y, odom_data.pose.pose.orientation.z, odom_data.pose.pose.orientation.w);
+	tf::Matrix3x3 m(q);
+	pose_x = odom_data.pose.pose.position.x;
+	pose_y = odom_data.pose.pose.position.y;
+	m.getRPY(roll, pitch, yaw);
+	}
 	
-	};
+/* Odometry subscriber function */
+void Node::odomSub(){
+	uint32_t queue_size = 1;
+	odom = nh.subscribe<nav_msgs::Odometry>("/pose",queue_size,&Node::odomCallbk,this);
+	}	
 
-//Property types 
-typedef boost::property<boost::edge_weight_t,int> EdgeWeightProperty;
-typedef boost::property<boost::vertex_name_t,std::string,boost::property<boost::vertex_index2_t,int> > VertexProperties;
+pcl::PointCloud<pcl::PointXYZ> Node::randomSample(pcl::PointCloud<pcl::PointXYZ> in_cld){
+	// Randomly sample 1000 pts from the cloud to calculate 2d rigit transform
+	pcl::PointCloud<pcl::PointXYZ>::Ptr in_cld_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+	*in_cld_ptr = in_cld;
+	pcl::RandomSample<pcl::PointXYZ> sample(true);
+	sample.setInputCloud(in_cld_ptr);
+	sample.setSample(5000);  // 1000 pts
+	std::vector<int> out_idx;
+	sample.filter(out_idx);
+	pcl::PointCloud<pcl::PointXYZ> out_cld;
+	sample.filter(out_cld);
+	std::cout<<out_cld.size()<<std::endl;
+	return out_cld;
+}
 
-//Graph type 
-//typedef boost::adjacency_list<boost::vecS,boost::vecS, boost::undirectedS,VertexProperties, EdgeWeightProperty> Graph;
-typedef boost::adjacency_list<boost::listS,boost::vecS, boost::undirectedS,Pose, constraints> Graph;
-
-// create hte vertices
-typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
-
-//create edges 
-typedef boost::graph_traits<Graph>::edge_descriptor Edge;
-
-Graph g;
-Eigen::Matrix4f tr_mat;
-double roll, pitch, yaw;
-double pose_x,pose_y;
-cloud_seq cloud_seq_loaded; 
-pose_seq pose_seq_loaded;
-const float res = 0.1;
-pcl::PointCloud<pcl::PointXYZ> static_pc;
-int count = 0;
-
-void createGraph(const Eigen::Matrix4f tr_msg){
-	
-	//Iterate over the graph 
-	Graph::vertex_iterator vertexIt,vertexEnd;
-	Graph::adjacency_iterator neighbourIt, neighbourEnd;
-	
-	
-	boost::tie(vertexIt, vertexEnd) = boost::vertices(g);
-	
-	
-	
-	Vertex v1 ;
-	v1 = boost::add_vertex(g);
-	g[v1].key = count;
-	g[v1].data  = pose_seq_loaded[1];
-	count++;
-	
-	
-	
-	Edge e1; 
-	e1 = (boost::add_edge(v1,*(vertexEnd-1),g)).first;
-	g[e1].transformation = tr_msg;
-	
-	
-	
-	
-	
-	/*
-	for(;vertexIt!=vertexEnd;++vertexIt)
-	{
-		//std::cout<<g[*vertexIt].key<<"is connected with";
-		boost::tie(neighbourIt,neighbourEnd) = boost::adjacent_vertices(*vertexIt,g);
-		for(;neighbourIt != neighbourEnd;++neighbourIt)
-		//std::cout<<g[*neighbourIt].key<<" ";
-		std::cout<<std::endl;
-		}
-	Graph::edge_iterator edgeIt,edgeEnd;
-	boost:tie(edgeIt,edgeEnd) = boost::edges(g);
-	for(;edgeIt!=edgeEnd;++edgeIt){
-		//std::cout<<g[*edgeIt].transformation<<std::endl;
-		std::cout<<std::endl;
-		}
+Eigen::Matrix4f Node::estTrans(pcl::PointCloud<pcl::PointXYZ> first,pcl::PointCloud<pcl::PointXYZ> second){
+	/* Error that the size of both the cloud should be same
+		Solution: randomly sample same number of points and get transform for those
+			Estimating 2D transform between cloud points
 	*/
 	
-	}
-
-void getOdom(nav_msgs::Odometry data){
-	/*
-	 *This function gets the odometry msg from the robot 
-	 * and using TF gives the angle in euclidean form. 
-	 * 
-	 */
-	
-	//std::cout<<"Receiving odometry"<<std::endl;
-	//std::cout<< data.pose.pose.orientation<<std::endl;
-	tf::Quaternion q(data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w);
-	tf::Matrix3x3 m(q);
-
-	pose_x = data.pose.pose.position.x;
-	pose_y = data.pose.pose.position.y;
-	m.getRPY(roll, pitch, yaw);
-
-	//std::cout << "x: " << pose_x << ", y : " << pose_y << ", Yaw: " << yaw << std::endl;
-	//std::cout<< v[1];	
-	}
-		
-void processCloud(const sensor_msgs::PointCloud2 msg)
-{
-	std::cout<<"Receiving cloud"<<std::endl;
-	std::cout << "x: " << pose_x << ", y : " << pose_y << ", Yaw: " << yaw << std::endl;
-	double myPoses[] = {pose_x,pose_y,yaw};
-	std::list<double> curr_pose (myPoses, myPoses + sizeof(myPoses) / sizeof(double) );
-	
-	pcl::PointCloud<pcl::PointXYZ>::Ptr curr_pc (new pcl::PointCloud<pcl::PointXYZ>);
-	pcl::PCLPointCloud2 cloud;
-	pcl::PointCloud<pcl::PointXYZ> pcl_pc;
-	std::vector<int> nan_indices;
-	pcl::PointCloud<pcl::PointXYZ>::Ptr prev_pc (new pcl::PointCloud<pcl::PointXYZ>); 
-		
-	
-	//********* Retirive and process raw pointcloud************
-	pcl_conversions::toPCL(msg,cloud);
-	pcl::fromPCLPointCloud2(cloud,pcl_pc);
-	pcl::removeNaNFromPointCloud(pcl_pc,pcl_pc,nan_indices);
-	*curr_pc =pcl_pc;
-	
-	//***************Filter point cloud to detect nearby changes only *****************
-		pcl::PassThrough<pcl::PointXYZ> pass;
-		pass.setInputCloud (curr_pc);
-		pass.setFilterFieldName ("z");
-		pass.setFilterLimits (0.0, 3.0);
-		pass.filter (*curr_pc);
-		
-		pcl::StatisticalOutlierRemoval<pcl::PointXYZ> dy_sor;
-		dy_sor.setInputCloud (curr_pc);
-		dy_sor.setMeanK (20);
-		dy_sor.setStddevMulThresh (1.0);
-		dy_sor.filter (*curr_pc);
-	
-	cloud_seq_loaded.push_back(pcl_pc);
-	pose_seq_loaded.push_back(curr_pose);
-	std::cout<<pose_seq_loaded.size()<<std::endl;
-	
-	if(cloud_seq_loaded.size()>2){
-		cloud_seq_loaded.pop_front();
-		pose_seq_loaded.pop_front();
-	
-	}
-	if(cloud_seq_loaded.size()==1){
-		//Add first vertex ;
-		Vertex v;
-		v = boost::add_vertex(g);
-		g[v].key = count;
-		g[v].data  = pose_seq_loaded[0];
-		count++;
-		}
-	if(cloud_seq_loaded.size()==2)
-	{
-		pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-		pcl::PointCloud<pcl::PointXYZ>::Ptr first_pc (new pcl::PointCloud<pcl::PointXYZ>);
-		pcl::PointCloud<pcl::PointXYZ>::Ptr second_pc (new pcl::PointCloud<pcl::PointXYZ>);
-		*first_pc = cloud_seq_loaded[0];
-		*second_pc = cloud_seq_loaded[1];
-		icp.setInputSource(second_pc);
-		icp.setInputTarget(first_pc);
-		pcl::PointCloud<pcl::PointXYZ> Final;
-		icp.align(Final);
-		//std::cout << "has converged:" << icp.hasConverged() << " score: " <<
-		//icp.getFitnessScore() << std::endl;
-		//std::cout << icp.getFinalTransformation() << std::endl;
-		tr_mat = icp.getFinalTransformation();
-		std::cout<<tr_mat<<std::endl;
-		createGraph(tr_mat);
-		std::cout<<std::endl;
-		boost::write_graphviz(std::cout, g);
-		std::cout<<std::endl;
-		}
-		
+	pcl::registration::TransformationEstimation2D< pcl::PointXYZ, pcl::PointXYZ, float >  ddTr; 	
+	pcl::PointCloud<pcl::PointXYZ>::Ptr first_pc (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr second_pc (new pcl::PointCloud<pcl::PointXYZ>);
+	*first_pc = this->randomSample(first);
+	*second_pc = this->randomSample(second);
 	 
+	Eigen::Matrix4f result;
+	ddTr.estimateRigidTransformation(*first_pc,*second_pc,result);
+	//std::cout<<result<<std::endl;
+	return result;
+}
 	
-	std::cout<<"finished"<<std::endl;
-	std::cout<<std::endl;	
+void Node::cloudCallbk(sensor_msgs::PointCloud2 msg){
+	std::vector<int> nan_indices;
+	pcl_conversions::toPCL(msg,cloud_);
+	pcl::fromPCLPointCloud2(cloud_,curr_pc);
+	pcl::removeNaNFromPointCloud(curr_pc,curr_pc,nan_indices);
+	
+	}
+void Node::cloudSub(){
+	uint32_t queue_size = 1;
+	cloud = nh.subscribe<sensor_msgs::PointCloud2>("/camera/depth/points",queue_size,&Node::cloudCallbk, this);
+	}
+
+
+/* calculate distance travelled */ 
+float Node::calculateDist(vector<double> curr_odom,vector<double> prev_odom){
+	float dist;	
+	dist = pow((curr_odom[0] - prev_odom[0]),2) + pow((curr_odom[1]-prev_odom[1]),2);
+	dist = sqrt(dist);
+	return dist;
+	}
+		
+/* Add vertex to graph with given data */ 
+void Node::addVertex(int v,std::vector<double> odom){
+	Vertex v1;
+	v1 = boost::add_vertex(gr);
+	gr[v1].key = v ;
+	gr[v1].data = odom;
+	return;
+	}
+		
+/* Add edge to the last added vertex */ 
+void Node::addEdge(Eigen::Matrix4f tr_msg){
+	//Graph::vertex_iterator vertex_It,vertex_End;
+	boost::tie(vertex_It, vertex_End) = boost::vertices(gr);
+	//std::cout<< "-->Vertex End is :"<<*vertexEnd<<std::endl;
+	Edge e1; 
+	e1 = (boost::add_edge(*(vertex_End-1),(*vertex_End),gr)).first;
+	gr[e1].transformation = tr_msg;
+	gr[e1].src = gr[*vertex_End-1].key;
+	gr[e1].obs = gr[*vertex_End].key;
+	}
+		
+/* Initialize graph */ 
+void Node::initGraph(int v,std::vector<double> odom){
+	Vertex v1;
+	v1 = boost::add_vertex(gr);
+	gr[v1].key = v ;
+	gr[v1].data = odom;
+	}
+		
+/* main node process */ 
+Node::Node(){
+	pcl::PointCloud<pcl::PointXYZ> prev_cld; // one step buffer for clouds comparision
+	int count = 1; // conter for key of the vertex
+	threshold_distance=0.5; // thesholding for the distance travelled
+	/* Subscribe to odometry */ 
+	this->odomSub(); // data-> pose_x,pose_y,roll, pitch, yaw
+	/* Subscribe to point cloud */
+	this->cloudSub(); //data-> curr_pc
+	
+	/* Initialize graph with first vertex as the starting point */
+	std::vector<double> curr_odom;
+	curr_odom.push_back(pose_x);
+	curr_odom.push_back(pose_y);
+	curr_odom.push_back(yaw);
+	this->initGraph(0,curr_odom); //data-> gr graph
+	
+	/* Save the first cloud */ 
+	prev_cld = curr_pc;
+	/* Whle the process is going on loop it */
+	while(ros::ok()){
+		/*subscribe to odom */ 
+		this->odomSub();
+		/* Subscribe to point cloud */ 
+		this->cloudSub();
+		/* Calculate distance */
+		float distance;
+		std::vector<double> now_odom;
+		curr_odom.push_back(pose_x);
+		curr_odom.push_back(pose_y);
+		curr_odom.push_back(yaw);
+		boost::tie(vertex_It,vertex_End) = boost::vertices(gr);
+		std::vector<double> prev_odom;
+		prev_odom = gr[*vertex_End].data;
+		distance = this->calculateDist(now_odom, prev_odom);
+		
+		/* if distance is greater than threshold */
+		if(distance >= threshold_distance){
+			/*  add vertex to graph  */
+			this->addVertex(count,now_odom);
+			/* calculate transform */
+			tr_mat =this->estTrans(curr_pc,prev_cld);
+			/* Add edge weights between current and previous */
+			this->addEdge(tr_mat); 
+			prev_cld = curr_pc;
+			count++;
+		}
+		
+		ros::spinOnce();
+		}
+		boost::write_graphviz(cout, gr);
 	}
 
 
 int main(int argc, char **argv)
 {
+	/*
+	 * Initialize Ros components. Subscribe to point cloud , odometry.
+	 */ 
+	 
+	ros::init(argc,argv,"graph_node");
+
+	Node graphNode = Node();	
+	/* Write data to file */
+
 	
-	ros::init(argc, argv, "pose_graph");
-	std::cout<<"Create object of posegraph"<<std::endl;
-	ros::NodeHandle nh;//create handle
-	uint32_t queue_size = 1;
-	//ros::Subscriber sub_pose = nh.subscribe<nav_msgs::Odometry>("/odom",queue_size,getOdom);
-	ros::Subscriber sub_pose = nh.subscribe<nav_msgs::Odometry>("/pose",queue_size,getOdom);
-	
-	ros::Subscriber sub_cloud = nh.subscribe<sensor_msgs::PointCloud2>("/camera/depth/points",queue_size,processCloud);
-	ros::spin();	
 	return 0;
 }
 
